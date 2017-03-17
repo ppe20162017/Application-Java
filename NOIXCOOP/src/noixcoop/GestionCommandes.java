fd125c92eee6cb232fc414ca11cbf99156cab6ab
ppe20162017 <tanguy.equinet@gastonberger.fr>
2017-03-17 11:17:53 +0100

    Création de la classe Gestion Commande

---
+++ NOIXCOOP/src/noixcoop/GestionCommandes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package noixcoop;
+
+/**
+ *
+ * @author benoi
+ */
+public class GestionCommandes {
+    private PersistanceSQL données;
+
+    public GestionCommandes(PersistanceSQL données) {
+        this.données = données;
+    }
+    
+    public Distributeur getDistributeur (String idDistributeur){
+        
+    }
+    
+    public String XmlNonLivrees (Distributeur unDistributeur){
+        
+    }
+    
+}
