de51de3b59d95f0df2f11c6061efe1fc86246721
ppe20162017 <tanguy.equinet@gastonberger.fr>
2017-03-19 15:07:11 +0100

    Class Fichier pour ecrire le xml dedans + methode XmlNonLivrees

--- NOIXCOOP/src/noixcoop/GestionCommandes.java
+++ NOIXCOOP/src/noixcoop/GestionCommandes.java
@@ -6,6 +6,7 @@
 package noixcoop;
 
 import java.util.ArrayList;
+import java.io.IOException;
 
 /**
  *
@@ -29,8 +30,22 @@ public class GestionCommandes {
         return leDistributeur; 
     }
     
-    public String XmlNonLivrees (Distributeur unDistributeur){
-        
+    public Fichier XmlNonLivrees (Distributeur unDistributeur) throws IOException, ClassNotFoundException{
+        Fichier unFichier = new Fichier();
+        unFichier.ouvrir("E");
+        unFichier.ecrire("<?xml version=\"1.0\"encoding=\"UTF-8\"?>");
+        unFichier.ecrire("<commandes idDistributeur=\""+unDistributeur.getId()+"\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
+        for (Commande uneCommande : unDistributeur.getCommandesEnCours()){
+            unFichier.ecrire("<commande id=\""+uneCommande.getId()+"\"> ");
+            unFichier.ecrire("<produit variete=\""+uneCommande.getLeProduit().getVariete()+"\" type=\""+uneCommande.getLeProduit().getType()+"\" calibre=\""+uneCommande.getLeProduit().getCalibre()+"\" /> ");
+            unFichier.ecrire("<conditionnement type=\""+uneCommande.getConditionnement()+"\" /> ");
+            unFichier.ecrire("<quantite>"+uneCommande.getQuantite()+"</quantite>");
+            unFichier.ecrire("<date_conditionnement>"+uneCommande.getDateConditionnement()+"</date_conditionnement>");
+            unFichier.ecrire("<date_envoi>"+uneCommande.getDateEnvoi()+"</date_envoi>");
+            unFichier.ecrire("</commande>");
+        }
+        unFichier.ecrire("</commandes> ");
+        return unFichier;
     }
     
 }
